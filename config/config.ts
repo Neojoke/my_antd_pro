0be34d527fce6b99a89909787b5e439da83e3389
wupeng01 <wupeng01@ppdai.com>
2020-08-28 15:05:42 +0800

    1. first commit

---
+++ config/config.ts
@@ -0,0 +1,114 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+import defaultSettings from './defaultSettings';
+import proxy from './proxy';
+const { REACT_APP_ENV } = process.env;
+export default defineConfig({
+  hash: true,
+  antd: {},
+  dva: {
+    hmr: true,
+  },
+  layout: {
+    name: 'Ant Design Pro',
+    locale: true,
+    siderWidth: 208,
+  },
+  locale: {
+    // default zh-CN
+    default: 'zh-CN',
+    // default true, when it is true, will use `navigator.language` overwrite default
+    antd: true,
+    baseNavigator: true,
+  },
+  dynamicImport: {
+    loading: '@/components/PageLoading/index',
+  },
+  targets: {
+    ie: 11,
+  },
+  // umi routes: https://umijs.org/docs/routing
+  routes: [
+    {
+      path: '/user',
+      layout: false,
+      routes: [
+        {
+          name: 'login',
+          path: '/user/login',
+          component: './user/login',
+        },
+        {
+          name: '注册结果页',
+          icon: 'smile',
+          path: '/userregisterresult',
+          component: './UserRegisterResult',
+        },
+      ],
+    },
+    {
+      path: '/welcome',
+      name: 'welcome',
+      icon: 'smile',
+      component: './Welcome',
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      icon: 'crown',
+      access: 'canAdmin',
+      component: './Admin',
+      routes: [
+        {
+          path: '/admin/sub-page',
+          name: 'sub-page',
+          icon: 'smile',
+          component: './Welcome',
+        },
+      ],
+    },
+    {
+      name: '搜索列表（应用）',
+      icon: 'smile',
+      path: '/listsearchapplications',
+      component: './ListSearchApplications',
+    },
+    {
+      name: 'list.table-list',
+      icon: 'table',
+      path: '/list',
+      component: './ListTableList',
+    },
+    {
+      path: '/',
+      redirect: '/welcome',
+    },
+    {
+      name: '分析页',
+      icon: 'smile',
+      path: '/dashboardanalysis',
+      component: './DashboardAnalysis',
+    },
+    {
+      name: '成功页',
+      icon: 'smile',
+      path: '/resultsuccess',
+      component: './ResultSuccess',
+    },
+    {
+      component: './404',
+    },
+  ],
+  // Theme for antd: https://ant.design/docs/react/customize-theme-cn
+  theme: {
+    // ...darkTheme,
+    'primary-color': defaultSettings.primaryColor,
+  },
+  // @ts-ignore
+  title: false,
+  ignoreMomentLocale: true,
+  proxy: proxy[REACT_APP_ENV || 'dev'],
+  manifest: {
+    basePath: '/',
+  },
+});
